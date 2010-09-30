ed51b83c7da55c14663eb9ea76463183ed680a8e
Brandonian <brandon@tfnswebdesign.com>
2010-09-30 11:35:07 -0400

    alltests should only have resources and users exposed ATM

--- alltests.php
+++ alltests.php
@@ -9,7 +9,7 @@ require('tests/CEERNResourceUtil.php');
 class AllTests extends TestSuite {
   function AllTests() {
     $this->TestSuite('All tests');
-    //$this->addFile('tests/test_users.php');
+    $this->addFile('tests/test_users.php');
     $this->addFile('tests/test_resources.php');
 		//$this->addFile('tests/test_sources.php');
 		//$this->addFile('tests/test_education.php');
